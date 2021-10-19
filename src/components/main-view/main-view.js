91fe911b1a6780983201b6ce4e532df574c5e333
marcelogsoria <marcelogsoria@gmail.com>
2021-10-19 10:00:06 -0300

    Add files via upload

--- src/components/main-view/main-view.js
+++ src/components/main-view/main-view.js
@@ -1,15 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { useIntl } from "react-intl";
-import { Route, Switch, Link } from "react-router-dom";
-import { ListIcon, TableIcon } from "@commercetools-uikit/icons";
-import Text from "@commercetools-uikit/text";
-import Spacings from "@commercetools-uikit/spacings";
-import FlatButton from "@commercetools-uikit/flat-button";
-import ViewOne from "../view-one";
-import ViewTwo from "../view-two";
-import messages from "./messages";
-import styles from "./main-view.mod.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
+import { Route, Switch } from 'react-router-dom';
+import Text from '@commercetools-uikit/text';
+import Spacings from '@commercetools-uikit/spacings';
+import ViewOne from '../view-one';
+import messages from './messages';
+import styles from './main-view.mod.css';
 
 const MainView = (props) => {
   const intl = useIntl();
@@ -18,31 +15,15 @@ const MainView = (props) => {
     <Spacings.Inset scale="m">
       <Spacings.Stack scale="m">
         <Text.Headline as="h1" intlMessage={messages.title} />
-        <div className={styles["nav-header"]}>
-          <Spacings.Inline scale="s">
-            <FlatButton
-              as={Link}
-              to={`${props.match.url}/one`}
-              icon={<ListIcon />}
-              label={intl.formatMessage(messages.labelLinkOne)}
-            />
-            <FlatButton
-              as={Link}
-              to={`${props.match.url}/two`}
-              icon={<TableIcon />}
-              label={intl.formatMessage(messages.labelLinkTwo)}
-            />
-          </Spacings.Inline>
-        </div>
+        <div className={styles['nav-header']}></div>
         <Switch>
-          <Route path={`${props.match.path}/one`} component={ViewOne} />
-          <Route path={`${props.match.path}/two`} component={ViewTwo} />
+          <Route path={`${props.match.path}`} component={ViewOne} />
         </Switch>
       </Spacings.Stack>
     </Spacings.Inset>
   );
 };
-MainView.displayName = "MainView";
+MainView.displayName = 'MainView';
 MainView.propTypes = {
   match: PropTypes.shape({
     path: PropTypes.string.isRequired,
